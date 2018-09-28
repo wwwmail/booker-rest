c4a01aee093025ba54a4f4237521ca001682f9fd
dev dev <www@mail.cz>
2018-09-28 21:16:47 +0300

    28geek

--- tests/RoomsServiceTest.php
+++ tests/RoomsServiceTest.php
@@ -8,79 +8,119 @@ use PDO;
 use Application\Database\Connection;
 use Application\Database\RoomsService;
 use Application\Entity\Rooms;
-
-class RoomsServiceTest extends TestCase
+use tests\TrunsactionFiendlyDatabaseTestCase;
+//class RoomsServiceTest extends \PHPUnit_Extensions_Database_TestCase
+class RoomServiceTest extends TrunsactionFiendlyDatabaseTestCase
 {
-        //use TestCaseTrait;
-    use PHPUnit_Extensions_Database_TestCase_Trait;
 
-    // инстанцировать только pdo один во время выполнения тестов для очистки/загрузки фикстуры
-    static private $pdo = null;
-
-    // инстанцировать только PHPUnit\DbUnit\Database\Connection один раз во время теста
-    private $conn = null;
-    
-
-
-    static $room; //model новой комнаты
-    private static $obj; //obj new room
-
-
-    public function getConnection()
+   function __construct($name = null, array $data = array(), $dataName = '')
     {
-        if ($this->conn === null) {
-            if (self::$pdo === null) {
-                self::$pdo = new PDO( $GLOBALS['DB_DSN'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD'] );
-            }
-            $this->conn = $this->createDefaultDBConnection(self::$pdo, $GLOBALS['DB_DBNAME']);
+        parent::__construct($name, $data, $dataName);
+
+
+        $this->pdo = new PDO( $GLOBALS['DB_DSN'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD'] );
+        $this->pdo->exec('SET foreign_key_checks = 0'); 
+        $this->con = $this->createDefaultDBConnection($this->pdo, 'php');
+
+        $this->pdo->beginTransaction();
+    }
+
+
+private $pdo = null;
+
+private $con = null;
+
+
+public function getConnection()
+{
+    return $this->con;
+}
+
+
+
+static $room; //model 
+private static $obj; //obj i
+
+public function getConnection3()
+{
+    $pdo = new PDO( $GLOBALS['DB_DSN'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD'] );
+
+    return $this->createDefaultDBConnection($pdo, 'php');
+}    
+public function getConnection2()
+{
+    if ($this->conn === null) {
+        if (self::$pdo === null) {
+            self::$pdo = new PDO( $GLOBALS['DB_DSN'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD'] );
+            self::$pdo->beginTransaction();
         }
+        self::$pdo->exec('SET foreign_key_checks = 0');  
 
-        return $this->conn;
-    }
-    
-    protected function getDataSet()
-    {
-        return $this->createFlatXMLDataSet(dirname(__FILE__). '/data/app_room.xml');
-    }
-    
-    
-    public static function setUpBeforeClass()
-    {
-        $dbConf = array(
-            'host' => 'localhost',
-            'dbname' => $GLOBALS['DB_DBNAME'],
-            'user' => $GLOBALS['DB_USER'],
-            'pwd' => $GLOBALS['DB_PASSWD'],
-            'errmode' => 1,
-            'driver' => 'mysql',
-        );
-        $db = new Connection($dbConf);
+        $this->conn = $this->createDefaultDBConnection(self::$pdo, $GLOBALS['DB_DBNAME']);
 
-        
-        $room['name'] = 'room 1';
-        
+    }
 
-        self::$room = new RoomsService($db);
-        self::$obj = new Rooms();
-    }
-    
-    public function testReadAllGetRooms()
-    {
-        $this->assertNotEmpty(static::$room->fetchAll());
-    }
-    
-    public function testSaveNewTrue()
-    {
-        $testArray = ['name'=>'test room name', 'id'=>''];
-        $testArray = $this->getDataSet();
-        $test =  Rooms::arrayToEntity($testArray,  new Rooms());
+    self::$pdo->beginTransaction();
 
-        $this->assertTrue(static::$room->save($test));
-    }
-    
-    public function saveNew()
-    {
-        $query  = $this->connectDb()->prepare("INSERT INTO app_room (`name`) VALUES (?) ");
-        return $query->execute([$this->name]);
-    }
+    return $this->conn;
+}
+
+
+
+protected function getDataSet()
+{
+    return $this->createFlatXMLDataSet(dirname(__FILE__). '/data/app_rooms.xml');
+}
+
+
+public function tearDown()
+{
+    $this->pdo->rollBack();
+    $this->pdo->beginTransaction();
+}
+
+public static function setUpBeforeClass()
+{
+    $dbConf = array(
+        'host' => 'localhost',
+        'dbname' => $GLOBALS['DB_DBNAME'],
+        'user' => $GLOBALS['DB_USER'],
+        'pwd' => $GLOBALS['DB_PASSWD'],
+        'errmode' => 1,
+        'driver' => 'mysql',
+    );
+    $db = new Connection($dbConf);
+
+
+    $room['name'] = 'room 1';
+
+    //      self::$trait = new TestCaseTrait();
+    self::$room = new RoomsService($db);
+    self::$obj = new Rooms();
+}
+
+public function testReadAllGetRooms()
+{
+    $this->assertNotEmpty(static::$room->fetchAll());
+}
+
+public function testSaveNewTrue()
+{
+    $testArray = ['name'=>'test room name', 'id'=>''];
+    $testArray = $this->getDataSet();
+    $test =  Rooms::arrayToEntity($testArray,  new Rooms());
+
+    $this->assertTrue(static::$room->save($test));
+}
+
+public function saveNew()
+{
+    $query  = $this->connectDb()->prepare("INSERT INTO app_room (`name`) VALUES (?) ");
+    return $query->execute([$this->name]);
+}
+
+public function __destruct()
+{
+  $this->pdo->rollBack();
+}
 }
---
+++ tests/TrunsactionFiendlyDatabaseTestCase.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace tests;
+
+abstract class TrunsactionFiendlyDatabaseTestCase extends  \PHPUnit_Extensions_Database_TestCase
+
+{
+    /**
+     *      * Returns the database operation executed in test setup.
+     *           * Return DeleteAll and Insert After this.
+     *                *
+     *                     * @return PHPUnit_Extensions_Database_Operation_DatabaseOperation
+     *                          */
+    protected function getSetUpOperation()
+    {
+        return new \PHPUnit_Extensions_Database_Operation_Composite(
+            array
+            (
+                \PHPUnit_Extensions_Database_Operation_Factory::DELETE_ALL(),
+                \PHPUnit_Extensions_Database_Operation_Factory::INSERT()
+            )
+        );
+    }
+}
